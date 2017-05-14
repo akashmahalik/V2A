d03b35af62b449f8daf2ba1fdfa06e9f43fce68b
Akash Mahalik <parthoprakashmahalik@gmail.com>
2017-05-14 20:14:06 +0530

    Added recursively going through all sub-dir

--- v2a.py
+++ v2a.py
@@ -17,7 +17,10 @@ def wav2mp3(file,flag,filePath):
 	
 	file_name,extension = os.path.splitext(file)
 	if flag:
-		conversion2mp3 = 'lame ' +filePath+'/' +"\"%s\".wav" %file_name  + ' '+'Audio_files/'+ "\"%s\"" %file_name + '.mp3'
+		
+		conversion2mp3 = 'lame ' +filePath+'/' +"\"%s\".wav" %file_name  + ' '+'Audio_files/'+ filePath+'/'+ "\"%s\"" %file_name + '.mp3'
+		
+		
 	else:
 		conversion2mp3 = 'lame ' + "\"%s\".wav" %file_name  + ' '+'Audio_files/'+ "\"%s\"" %file_name + '.mp3'	
 	
@@ -28,15 +31,29 @@ def rest2mp3(file,flag,filePath):
 	
 	file_name,extension = os.path.splitext(file)
 	if flag:
-		conversion2wav = 'ffmpeg -i ' + filePath +'/' +'\"%s\"' %file_name + extension + ' ' + "\"%s\"" %file_name + '.wav'
+		conversion2wav = 'ffmpeg -i ' + filePath +'/' +'\"%s\"' %file_name + extension + ' ' + filePath+'/'+ "\"%s\"" %file_name + '.wav'
 	else:
 		conversion2wav = 'ffmpeg -i ' + '\"%s\"' %file_name + extension + ' ' + "\"%s\"" %file_name + '.wav'	
 	
 	
 	os.system(conversion2wav)
-	wav2mp3(file,0,filePath)
-	delete_wav = 'rm ' + "\"%s\"" %file_name + '.wav'
+	wav2mp3(file,1,filePath)
+	delete_wav = 'rm ' +filePath+'/'+ "\"%s\"" %file_name + '.wav'
 	os.system(delete_wav)
+
+def recur_dir(filePath):
+	for file in os.listdir(filePath):
+						print(file)
+						temp=filePath+'/'+file
+
+						if os.path.isdir(temp):
+							os.system('mkdir Audio_files/'+temp)
+							recur_dir(temp)
+						else:
+							v2a(file,1,filePath)	
+
+						
+						
 
 
 def main():
@@ -45,18 +62,18 @@ def main():
 		exit(1)
 	else:
 		filePath = sys.argv[1]
-
+		
 		try:
 			if os.path.exists(filePath):
 				print('Path exists :D')
 			else:
-				raise ValueError('File or Directory doesn\'t exist')
+				raise Exception('File or Directory doesn\'t exist')
 				exit(1)
 				
 		except OSError as error:
-				pass
+				raise error
 		
-		
+					
 		if os.path.isfile(filePath):
 					os.system('mkdir Audio_files')
 					print('Converting...')
@@ -69,16 +86,14 @@ def main():
 					
 
 		elif os.path.isdir(filePath):
-					os.system('mkdir Audio_files')
+					os.system('mkdir Audio_files/' + filePath)
 					print('Converting...')
 					
-					
-					for file in os.listdir(filePath):
-						print(file)
-						v2a(file,1,filePath)			
+					recur_dir(filePath)
 
 
-		
+
+
 
 
 
