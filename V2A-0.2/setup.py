0f6d184670863b0ebab0c34360a60d5cded87525
Akash Mahalik <parthoprakashmahalik@gmail.com>
2017-06-01 23:05:14 +0530

    Packing up files
    Making the program a package

---
+++ V2A-0.2/setup.py
@@ -0,0 +1,8 @@
+ #!/usr/bin/env python3
+from setuptools import setup
+ 
+setup(
+     name='V2A',    
+     version='0.2',                         ##Copies the existing mp3s
+     scripts=['v2a']                 
+)
