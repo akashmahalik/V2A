4aea204830e79ed3519e47112f69ab10a5451b42
Akash Mahalik <parthoprakashmahalik@gmail.com>
2017-06-04 23:02:52 +0530

    Update setup.py

--- V2A-0.2/setup.py
+++ V2A-0.2/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
  
 setup(
      name='V2A',    
-     version='0.2',                         ##Copies the existing mp3s
+     version='0.1',                         ##Copies the existing mp3s
      scripts=['v2a']                 
 )
